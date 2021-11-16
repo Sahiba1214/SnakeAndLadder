8a9eb5242ee3f514c92106e625a580ef06207cf9
sahiba <sahiba1214@gmail.com>
2021-11-16 23:46:40 +0530

    [sahiba1214] add : file added

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace SnakeAndLadder
 {
-    class Program
+    public class SnakeAndLadder
     {
-        static void Main(string[] args)
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING = 100, START = 0;
+        public int standingPositionofPlayer = 0;
+        Random random = new Random();
+        public int DieRolling()
         {
-            Console.WriteLine("Welcome TO SNAKE AND LADDER Game");
-            SnakeAndLadder playerOne = new SnakeAndLadder();
-            playerOne.DieRolling();
-            playerOne.PlayerCheck();
+            int roll = random.Next(1, 7);
+            return roll;
+        }
+        public void PlayerCheck()
+        {
+            while (standingPositionofPlayer < WINNING)
+            {
+                int dieRoll = this.DieRolling();
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case NO_PLAY:
+                        break;
+                    case LADDER:
+                        this.standingPositionofPlayer += dieRoll;
+                        break;
+                    case SNAKE:
+                        this.standingPositionofPlayer -= dieRoll;
+                        if (this.standingPositionofPlayer < START)
+                        {
+                            this.standingPositionofPlayer = START;
+                        }
+                        break;
+                }
+                //Console.WriteLine("The standin Position of Player is :" + this.standingPositionofPlayer + " " + "with a roll of " + dieRoll);
+            }
+            Console.WriteLine("The Player wins the ame");
         }
     }
 }
